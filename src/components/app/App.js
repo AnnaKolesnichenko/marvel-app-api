8b7e3e25425b13b1746ec7fa683931f9000522e1
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-07-26 17:25:44 +0300

    fix

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+// import { Route, Routes } from 'react-router-dom';
 
 import AppHeader from '../appHeader/AppHeader';
 import RandomChar from '../randomChar/RandomChar';
--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -4,17 +4,17 @@ const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <a href="#">
+                <a href="www.ex.com">
                     <span>Marvel</span> information portal
                 </a>
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><a href="#">Characters</a></li>
+                    <li><a href="www.ex.com">Characters</a></li>
                     /
-                    <li><a href="#">Comics</a></li>
+                    <li><a href="www.ex.com">Comics</a></li>
                     /
-                    <li><a href="#">Search</a></li>
+                    <li><a href="www.ex.com">Search</a></li>
                 </ul>
             </nav>
         </header>
