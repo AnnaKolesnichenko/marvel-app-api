e628ada6eb92d8c57fc1f4cb251bc855d451f01f
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-08-09 14:46:47 +0300

    Update SingleMarvelPage.js

--- src/components/pages/SingleMarvelPage.js
+++ src/components/pages/SingleMarvelPage.js
@@ -30,30 +30,30 @@ const SingleMarvelPage = ({ data }) => {
   }, [marvelId]);
 
   const { title, description, thumbnail } = data;
-  const poster = `${thumbnail.path}.${thumbnail.extension}`;
-
-  return (
-    <>
-      <AppBanner />
+    const poster = `${thumbnail.path}.${thumbnail.extension}`;
+    
+    return (
+        <>
+            <AppBanner />
       {loading ? (
         <Spinner />
       ) : (
-        <div className="single-comic">
-          <img
-            src={thumbnail ? poster : <p>Hello</p>}
-            alt={title}
-            className="single-comic__img"
-          />
-          <div className="single-comic__info">
-            <h2 className="single-comic__name">{title}</h2>
-            <p className="single-comic__descr">{description}</p>
-          </div>
-          <Link to="/characters" className="single-comic__back">
-            Back to all
-          </Link>
-        </div>
+            <div className="single-comic">
+                <img
+                    src={thumbnail ? poster : <p>Hello</p>}
+                    alt={title}
+                    className="single-comic__img"
+                />
+                <div className="single-comic__info">
+                    <h2 className="single-comic__name">{title}</h2>
+                    <p className="single-comic__descr">{description}</p>
+                </div>
+                <Link to="/characters" className="single-comic__back">
+                    Back to all
+                </Link>
+            </div>
       )}
-    </>
+        </>
   );
 };
 
